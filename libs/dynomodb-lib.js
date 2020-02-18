8de6bf6ec7b5b5ba97cd335ccc0e65d14695df12
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-02-18 20:38:38 +0530

    fix:typo

--- libs/dynomodb-lib.js
+++ libs/dynomodb-lib.js
@@ -3,5 +3,5 @@ import AWS from 'aws-sdk';
 export function call(action, params) {
     const dynomoDb = new AWS.DynamoDB.DocumentClient();
 
-    return dynomoDb[action](params).promise()
-}   
+    return dynomoDb[action](params).promise();
+}
